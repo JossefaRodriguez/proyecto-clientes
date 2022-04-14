787f08065cdfb2ee24c1cdab09d53c7542b6d21e
Rodrigo Palza Gonzalez <programaro07@gmail.com>
2022-04-13 22:46:49 -0400

    proyecto finalizado

--- script.sql
+++ script.sql
@@ -13,7 +13,7 @@ psql -U postgres sistema_ventas < C:\Users\rodri\Documents\src\modulo-5\proyecto
 
 -- Primera compra usuario01
 BEGIN;
-INSERT INTO compra (id, cliente_id, fecha) VALUES (33, 1, '2202-04-13');
+INSERT INTO compra (id, cliente_id, fecha) VALUES (33, 1, '2022-04-13');
 INSERT INTO detalle_compra (id, producto_id, compra_id, cantidad) VALUES (43, 9, 33, 5);
 SELECT stock FROM producto WHERE id = 9;
 UPDATE producto SET stock = stock - 5 WHERE id = 9;
@@ -22,7 +22,7 @@ COMMIT;
 
 -- Segunda compra usuario02
 BEGIN;
-INSERT INTO compra (id, cliente_id, fecha) VALUES (34, 2, '2202-04-13');
+INSERT INTO compra (id, cliente_id, fecha) VALUES (34, 2, '2022-04-13');
 INSERT INTO detalle_compra (id, producto_id, compra_id, cantidad) VALUES (44, 1, 34, 3);
 SELECT stock FROM producto WHERE id = 1;
 UPDATE producto SET stock = stock - 3 WHERE id = 1;
